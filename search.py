086b403b7e50a1d1beddbe95c1506747d6764685
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-01-20 11:14:07 +0530

    add logging

--- search.py
+++ search.py
@@ -1,7 +1,29 @@
 import os
 import argparse
+import logging
 from docx import Document
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+
+# add our own logging level
+SUCCESS = 25 
+logging.addLevelName(SUCCESS, "SUCCESS")
+def success(self, message, *args, **kwargs):
+    if self.isEnabledFor(SUCCESS):
+        self._log(SUCCESS, message, args, **kwargs)
+logging.Logger.success = success
+
+# configure logger
+log_format = '[%(levelname)s] %(asctime)s: %(message)s'
+log_file_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.log'
+log_file_path = os.path.join(os.getcwd(), log_file_name)
+
+logging.basicConfig(level=logging.DEBUG, format=log_format, handlers=[
+    logging.FileHandler(log_file_path),
+    logging.StreamHandler()
+])
+
+logger = logging.getLogger(__name__)
 
 def check(fpath, target):
     try:
@@ -11,16 +33,16 @@ def check(fpath, target):
                 return True
         return False
     except Exception as e:
-        print(f"Error processing {fpath}: {e}")
+        logger.error(f"Error processing {fpath}: {e}")
         return False
 
 def process_file(file):
     fname, target = file
     fpath = os.path.join(os.getcwd(), fname)
     if check(fpath, target):
-        print(f"'{target}' found in {fname}")
+        logger.success(f"'{target}' found in {fname}")
     else:
-        print(f"'{target}' not found in {fname}")
+        logger.info(f"'{target}' not found in {fname}")
 
 def main():
     parser = argparse.ArgumentParser(description='Search for a word in .docx files in the current directory.')
@@ -36,8 +58,7 @@ def main():
 if __name__ == "__main__":
     import time
     start_time = time.time()
-    main()  
+    main()
     end_time = time.time()
     execution_time = end_time - start_time
-    print(f"Execution Time: {execution_time} seconds")
-    
+    logger.info(f"Execution Time: {execution_time} seconds")
