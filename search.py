b18f7e87f67265ce3f2058c4b482ab2a55488b35
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-01-20 13:10:19 +0530

    add doc strings

--- search.py
+++ search.py
@@ -5,7 +5,7 @@ from docx import Document
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 
-# configure logger
+# Configure logger
 log_format = '(%(asctime)s) [%(levelname)s] %(message)s'
 log_file_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.log'
 log_file_path = os.path.join(os.getcwd(), log_file_name)
@@ -18,6 +18,16 @@ logging.basicConfig(level=logging.DEBUG, format=log_format, handlers=[
 logger = logging.getLogger(__name__)
 
 def check(fpath, target):
+    """
+    Check if the target word is present in the paragraphs of a given Word document.
+
+    @param fpath: The file path of the Word document.
+    @type fpath: str
+    @param target: The word to search for in the document.
+    @type target: str
+    @return: True if the word is found, False otherwise.
+    @rtype: bool
+    """
     try:
         doc = Document(fpath)
         for paragraph in doc.paragraphs:
@@ -25,27 +35,41 @@ def check(fpath, target):
                 return True
         return False
     except Exception as e:
-        logger.error(f"Error processing {fpath}: {e}")
+        logger.error("Error processing %s: %s" % (fpath, e))
         return False
 
 def process_file(file):
+    """
+    Process a Word document file, checking if a target word is present.
+
+    @param file: A tuple containing the file name and the target word.
+    @type file: tuple
+    @return: None
+    @rtype: None
+    """
     fname, target = file
     fpath = os.path.join(os.getcwd(), fname)
     if check(fpath, target):
-        logger.info(f"'{target}' found in {fname}")
+        logger.info("'%s' found in %s" % (target, fname))
     else:
-        logger.debug(f"'{target}' not found in {fname}")
+        logger.debug("'%s' not found in %s" % (target, fname))
 
 def main():
+    """
+    Main function to search for a word in .docx files in the current directory.
+
+    @return: None
+    @rtype: None
+    """
     parser = argparse.ArgumentParser(description='Search for a word in .docx files in the current directory.')
     parser.add_argument('word', type=str, help='The word to search for')
     args = parser.parse_args()
 
     target = args.word
-    fall = [(fname, target) for fname in os.listdir() if fname.endswith(".docx")]
+    file_list = [(fname, target) for fname in os.listdir() if fname.endswith(".docx")]
 
     with ThreadPoolExecutor() as executor:
-        executor.map(process_file, fall)
+        executor.map(process_file, file_list)
 
 if __name__ == "__main__":
     import time
@@ -53,4 +77,4 @@ if __name__ == "__main__":
     main()
     end_time = time.time()
     execution_time = end_time - start_time
-    logger.debug(f"Execution Time: {execution_time} seconds")
+    logger.debug("Execution Time: %s seconds" % execution_time)
