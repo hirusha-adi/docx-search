5482d88ffb87455a8040a8251c616f3e12ddff31
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-01-23 23:03:45 +0530

    format code

--- search.py
+++ search.py
@@ -2,14 +2,13 @@ import argparse
 import os
 from docx_search import docx_search
 
+
 def main():
     parser = argparse.ArgumentParser(description='Search for a word in .docx files in a specified directory.')
-    parser.add_argument(
-        '--dir', dest='target_dir', type=str, default=os.getcwd(),
-                        help='The target directory to search for .docx files. Defaults to the current working directory.'
-                        )
+    parser.add_argument('--dir', dest='target_dir', type=str, default=os.getcwd(),
+                        help='The target directory to search for .docx files. Defaults to the current working directory.')
     parser.add_argument('--word', dest='target_word', type=str,
-                        help='The word to search for in the documents. If not provided, user will be prompted.')
+                        help='The word to search for in the documents. If not provided, the user will be prompted.')
     args = parser.parse_args()
 
     target_dir = args.target_dir
@@ -19,6 +18,7 @@ def main():
         target_word = input("Enter the word to search for: ")
 
     docx_search(target_dir=target_dir, target_word=target_word)
+
 
 if __name__ == "__main__":
     main()
